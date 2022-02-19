c2714d3daef78fdbb1c5e0c749d03924efddc617
Onur ALTINTAŞ <onuraltintas@hotmail.com>
2022-02-19 09:45:38 +0300

    Add project files.

---
+++ ConsoleProgramlama/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleProgramlama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("İsminizi Girin: ");
+            string isim = Console.ReadLine();
+            Console.Write("Soyisminizi Girin: ");
+            string soyisim = Console.ReadLine();
+            Console.WriteLine("Merhaba " + isim + " " + soyisim);
+        }
+    }
+}
---
+++ Degiskenler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Degiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 5; //1 Byte
+            sbyte sb = 5; //1 Byte
+
+            short s = 5; //2 Byte
+            ushort us = 5; //2 Byte
+
+            Int16 i16 = 2; // 2 Byte
+            int i = 2; // 4 Byte
+            Int32 i32 = 3; // 4 Byte
+            Int64 i64 = 4; //8 Byte
+
+            uint ui = 2; // 4 Byte 
+            long l = 3; // 8 Byte
+
+            ulong ul = 4; // 8 Byte
+
+            float f = 5; // 4 Byte
+            double d = 5; // 8 Byte
+            decimal de = 5; // 16 Byte
+
+            char c = '5'; //2 Byte
+
+            string str = "Onur"; //sınırsız
+
+            bool b1 = true;
+            bool b2 = false;
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+
+        }
+    }
+}
