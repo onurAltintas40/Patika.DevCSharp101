9b41c00921634c780fed6bcff6f1316168a8efc0
Onur ALTINTAŞ <onuraltintas@hotmail.com>
2022-02-20 18:31:54 +0300

    Hata yönetimi örneği

---
+++ HataYonetimi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+
+            try
+            {
+                Console.Write("Bir sayı giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı: " + sayi);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hata: "+e.Message.ToString());                
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+
+            */
+            try
+            {
+                //int a = int.Parse(null);
+
+                int a = int.Parse("test");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);                
+            }
+
+        }
+    }
+}
