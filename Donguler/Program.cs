d1f7900244cae577db093b7bc9153c1e68ae5dd3
Onur ALTINTAŞ <onuraltintas@hotmail.com>
2022-02-21 20:09:50 +0300

    Döngüler eklendi.

---
+++ Donguler/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ekrandan girilen sayıya kadar olan tek sayıları yazdır.
+
+            int sayac = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine("Değer: " + i);
+                }
+            }
+
+            int tekToplam=0, ciftToplam=0;
+
+            for (int i = 0; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tekToplam += i;
+                }
+                else
+                {
+                    ciftToplam += i;
+                }
+            }
+
+            Console.WriteLine("Tek Toplam: " + tekToplam);
+            Console.WriteLine("Çift Toplam: " + ciftToplam);
+
+            //break, continue
+
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4) break;
+                Console.WriteLine(i);               
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4) continue;
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
