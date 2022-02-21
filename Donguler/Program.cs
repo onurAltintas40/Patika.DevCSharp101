2f0439816f3621e72a9ca3061318b2e1fb862748
Onur ALTINTAŞ <onuraltintas@hotmail.com>
2022-02-21 20:39:06 +0300

    While ve foreach döngü örnekleri eklendi.

--- Donguler/Program.cs
+++ Donguler/Program.cs
@@ -6,8 +6,10 @@ namespace Donguler
     {
         static void Main(string[] args)
         {
-            // Ekrandan girilen sayıya kadar olan tek sayıları yazdır.
+            //for Döngüsü
 
+            // Ekrandan girilen sayıya kadar olan tek sayıları yazdır.
+            /*
             int sayac = int.Parse(Console.ReadLine());
 
             for (int i = 0; i <= sayac; i++)
@@ -49,6 +51,39 @@ namespace Donguler
                 if (i == 4) continue;
                 Console.WriteLine(i);
             }
+            */
+
+            // While Döngüsü
+            // 1'den başlayarak girilen sayıya kadar (dahil) ortalama hesaplayıp konsola yazdıran program.
+
+            int sayi = int.Parse(Console.ReadLine());
+            int toplam = 0;
+            int sayac = 1;
+
+            while (sayac<=sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine("Ortalama:" + toplam / sayi);
+
+            char karakter= 'a';
+            while (karakter<'z')
+            {
+                Console.Write(karakter);
+                karakter++;
+            }
+
+            Console.WriteLine(karakter);
+
+            string[] arabalar = { "BMW", "Audi", "Mazda", "Opel" };
+
+            foreach (var item in arabalar)
+            {
+                Console.WriteLine(item);
+            }
+
 
         }
     }
