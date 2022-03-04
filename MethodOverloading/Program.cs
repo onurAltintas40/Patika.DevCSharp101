1e13a34d298da511c7698b44a44bb6a56affa5a2
Onur ALTINTAŞ <onuraltintas@hotmail.com>
2022-03-04 13:15:30 +0300

    Method overloading örnekleri yapıldı.

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // out parametreler
+
+            string sayi = "999";
+
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı - " + outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+
+            Metotlar ornek = new Metotlar();
+            ornek.Topla(2, 3, out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+
+
+            int ifade = 999;
+            ornek.EkranaYazdir(ifade.ToString());
+            ornek.EkranaYazdir("Onur Altıntaş");
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri.ToString());
+        }
+    }
+}
+
