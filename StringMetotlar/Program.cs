f85131accde117838310755190cd5027ebca4e9f
Onur ALTINTAŞ <onuraltintas@hotmail.com>
2022-03-06 10:03:32 +0300

    String metotları dersi örnekleri eklendi.

---
+++ StringMetotlar/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace StringMetotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string degisken = "Dersimiz CSharp, Hoş Geldiniz.";
+
+            string degisken2 = "Dersimiz CSharp";
+
+            Console.WriteLine(degisken.Length);
+
+            Console.WriteLine(degisken.ToLower());
+
+            Console.WriteLine(degisken.ToUpper());
+
+            Console.WriteLine(string.Concat(degisken, "Merhaba"));
+
+            Console.WriteLine(degisken.CompareTo(degisken2));
+
+            Console.WriteLine(String.Compare(degisken,degisken2,true));
+            Console.WriteLine(String.Compare(degisken, degisken2, false));
+
+            Console.WriteLine(degisken.Contains(degisken2));
+            Console.WriteLine(degisken.EndsWith("Hoş Geldiniz."));
+            Console.WriteLine(degisken.StartsWith("Merhaba"));
+
+            Console.WriteLine(degisken.IndexOf("CSharp"));
+
+            Console.WriteLine(degisken.Insert(0, "Merhaba"));
+
+            Console.WriteLine(degisken.LastIndexOf("i"));
+
+            Console.WriteLine(degisken + degisken2.PadLeft(30));
+            Console.WriteLine(degisken.PadRight(50) + degisken2);
+
+            Console.WriteLine(degisken + degisken2.PadLeft(30,'*'));
+            Console.WriteLine(degisken.PadRight(50,'-') + degisken2);
+
+            Console.WriteLine(degisken.Remove(10));
+            Console.WriteLine(degisken.Remove(5,3));
+            Console.WriteLine(degisken.Remove(0,1));
+
+            Console.WriteLine(degisken.Replace("CSharp", "C#"));
+            Console.WriteLine(degisken.Replace(" ", "*"));
+
+            Console.WriteLine(degisken.Split(' ')[1]);
+
+            Console.WriteLine(degisken.Substring(4));
+            Console.WriteLine(degisken.Substring(5, 10));
+
+
+
+
+        }
+    }
+}
