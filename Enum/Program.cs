20d82efdb4c304cd63650e11e9cf621f6f513a13
Onur ALTINTAŞ <onuraltintas@hotmail.com>
2022-03-14 15:02:02 +0300

    Enum örnekleri yapıldı.

---
+++ Enum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Pazar);
+
+            int sicaklik = 32;
+
+            if(sicaklik <= (int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için havanın ısınması bekle !!!");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Sıcak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak !!!");
+            }
+            if (sicaklik >= (int)HavaDurumu.Normal && sicaklik <= (int)HavaDurumu.ÇokSıcak)
+            {
+                Console.WriteLine("Hadi dışarıya çıkalım !!!");
+            }
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soğuk = 5,
+        Normal =20,
+        Sıcak =25,
+        ÇokSıcak = 30
+    }
+}
